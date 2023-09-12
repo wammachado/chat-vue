f56b257c9dfc14fd77a145a8d5a00b10dfeb1554
Wender Afonso <wammachado@gmail.com>
2023-09-11 22:04:58 -0300

    firebase

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import 'firebase/firebase-messaging'
 
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBxcrzplKWoLXl2sY33J_wE77YzqWVYCyc",
-    authDomain: "scriptador-df22c.firebaseapp.com",
-    databaseURL: "https://scriptador-df22c.firebaseio.com",
-    projectId: "scriptador-df22c",
-    storageBucket: "scriptador-df22c.appspot.com",
-    messagingSenderId: "665220623227",
-    appId: "1:665220623227:web:c4187b6004c3b575a82bde",
-    measurementId: "G-WVWCKRHSTX"
+    apiKey: "apiKey",
+    authDomain: "authDomain",
+    databaseURL: "databaseURL",
+    projectId: "projectId",
+    storageBucket: "storageBucket",
+    messagingSenderId: "messagingSenderId",
+    appId: "appId",
+    measurementId: "measurementId"
 
 
 }
